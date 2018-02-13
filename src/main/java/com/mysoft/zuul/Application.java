3c9d4b66a46f747f3bada74aed88f77984942234
JaviMora <javier.mora@linkeit.com>
2018-02-13 11:34:45 +0100

    first commit zuul

---
+++ src/main/java/com/mysoft/zuul/Application.java
@@ -0,0 +1,16 @@
+package com.mysoft.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/mysoft/zuul/filter/CustomZuulFilter.java
@@ -0,0 +1,36 @@
+package com.mysoft.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class CustomZuulFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        System.out.println(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
